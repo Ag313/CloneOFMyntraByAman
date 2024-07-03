6285c79faaee103fed066eb7a607d31afd6a2c20
Ag313 <ag88969@gmail.com>
2024-07-03 20:42:25 +0530

    PART OF FOURTH COMMIT

--- script/bag.js
+++ script/bag.js
@@ -3,31 +3,39 @@ onLoad();
 
 
 
-function onLoad(){
+function onLoad() {
     loadBagItemObject();
     displayBagItems();
 }
+// FUNCTIONALITY OF APPLYING LOOP FOR FETCHING ONE BY ONE  OBJECT/ITEM DETAILS BY ITS ID NO. DONE HERE...
+function loadBagItemObject() {
+    bagItemObjects = bagItem.map(itemId => {
+        for (let i = 0; i < items.length; i++) {
+            if (itemId == items[i].id) {
+                return items[i];
+            }
+        }
 
-function loadBagItemObject(){
-bagItemObjects= bagItem.map(itemId => {
-    for(let i=0; i < items.length;i++){
- if(itemId==items[i].id){
-    return items[i];
- }
-    }
-
-})
-console.log(bagItemObjects);
+    })
+    // console.log(bagItemObjects);
 }
 
-function displayBagItems(){
-
-
+function displayBagItems() {
     let containerElement = document.querySelector('.bag-items-container');
-    
-    // containerElement.innerHTML=
-function genrateItemHtml(item){
-return ` <div class="bag-item-container">
+
+
+    //  HERE... I'VE DONE THE PROCESS MEAN=> JO JO ITEM "GENERATEITEMHTML KI HELP SE AEGA USPE FOREACH LAGA DEGA JISSE ALAG ALAG DATA AJAE"
+    let innerHTML = '';
+    bagItemObjects.forEach(bagItem => {
+        innerHTML += genrateItemHtml(bagItem);
+    });
+
+    containerElement.innerHTML = innerHTML;
+
+    // FUNCTIONALITY FOR GENRATING ONE BY ONE OBJECT/ITEM DONE HERE...
+
+    function genrateItemHtml(item) {
+        return ` <div class="bag-item-container">
             <div class="item-left-part">
               <img class="bag-item-img" src="../${item.image}">
             </div>
@@ -37,7 +45,7 @@ return ` <div class="bag-item-container">
               <div class="price-container">
                 <span class="current-price">${item.current_price}</span>
                 <span class="original-price">${item.original_price}</span>
-                <span class="discount-percentage">(${item. discount_percentage}% OFF)</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
               </div>
               <div class="return-period">
                 <span class="return-period-days">${item.return_period} days</span> return available
@@ -50,6 +58,6 @@ return ` <div class="bag-item-container">
 
             <div class="remove-from-cart">X</div>
           </div>`
-}
+    }
 
 }
