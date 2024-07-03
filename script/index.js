de29be1679ddb275d3a4f6b0bd23be9cba590173
Ag313 <ag88969@gmail.com>
2024-07-03 16:49:01 +0530

    addbag functinality with css

--- script/index.js
+++ script/index.js
@@ -1,32 +1,44 @@
 let itemContainerElements=document.querySelector(".items-container")
 
-let item={
-    item_image:"images/1.jpg",
-    rating:{
-        rating_value:"4.5",
-        view:"1400"
-    },
-    company_name:"carlton London",
-    item_name:"Single Plated Kurti",
-    current_price:"606",
-    original_price:"1045",
-    discount:"42",
+
+let bagItem=[];
+displayItemsHomePage();
+
+
+
+function addToBag(itemId){
+bagItem.push(itemId);
+disolayBagItem();
+}
+function disolayBagItem(){
+let bagItemCountElement=document.querySelector(".bag-item-count");
+bagItemCountElement.innerText=bagItem.length;
 }
 
-itemContainerElements.innerHTML=`
- <div class="sub-item-container">
-                <img src="${item.item_image}" class="item-image" alt="item image"/>
-                <div class="rating">
-                    ${item.rating.rating_value}⭐ | ${item.rating.view}
-                </div>
-                <div>
-                    <div class="company-name">${item.company_name}</div>
-                    <div class="item-name">${item.item_name}</div>
-                </div>
-                <div class="price">
-                    <span class="current-price">₹ ${item.current_price}</span>
-                    <span class="original-price">₹${item.original_price}</span>
-                    <span class="discount">${item.discount}% OFF</span>
-                </div>
-                <button class="btn-add-bag">Add To Bag</button>
-            </div> `;
+function displayItemsHomePage(){
+    let itemsContainerElement = document.querySelector(".items-container");
+
+    let innerHTML="";
+    items.forEach(item => {
+        innerHTML+=`<div class="sub-item-container">
+                    <img src="${item.image}" class="item-image" alt="item image"/>
+                    <div class="rating">
+                        ${item.rating.stars}⭐ | ${item.rating.count}
+                    </div>
+                    <div>
+                        <div class="company-name">${item.company}</div>
+                        <div class="item-name">${item.item_name}</div>
+                    </div>
+                    <div class="price">
+                        <span class="current-price">₹ ${item.current_price}</span>
+                        <span class="original-price">₹${item.original_price}</span>
+                        <span class="discount">${item.discount_percentage}% OFF</span>
+                    </div>
+                    <button class="btn-add-bag" onclick="addToBag(${item.id})">Add To Bag</button>
+                </div> `;
+    });
+    
+    
+    itemContainerElements.innerHTML=innerHTML;
+}
+
